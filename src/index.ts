7d83d6285559511b0d02d25d54536f0e975659db
liming <liming@youxuepai.com>
2019-10-16 15:36:35 +0800

    oop

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,24 @@
-import { Pocker, Color, Num } from "./module"
-
-function createPocker(): Pocker {
-  let pocker: Pocker = [],
-    num = Object.values(Num),
-    color = Object.values(Color);
-
-  num.forEach(n => {
-    color.forEach(c => {
-      pocker.push({
-        color: c,
-        num: n
-      })
-    })
-  })
-  return pocker;
+// 抽象类棋子 chess
+abstract class Chess {
+    x: number = 0
+    y: number = 0
+    
+    abstract readonly name: string = '' // 抽象成员
 }
 
-function printPocker(pocker: Pocker) {
-  pocker.forEach(card => {
-    console.log(card.num + card.color)
-  })
+// 类 炮
+class Pao extends Chess {
+    readonly name: string = '炮' // 子类必须实现父类中的抽象成员
 }
-
-const pocker = createPocker()
-printPocker(pocker)
+class Horse extends Chess {
+    readonly name: string
+    constructor(){
+        super()
+        this.name = ''
+    }
+}
+class Soldier extends Chess {
+    get name () {
+        return '兵'
+    }
+}
